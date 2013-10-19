89d464df0726e09a5c30c00cf1e51e1c9dd4b0e8
Erik Beeson <erik.beeson@gmail.com>
2013-10-19 00:07:23 -0700

    Remove optional 'extractor' parameter. Now matches for any data-firebase* attribute and optionally accepts child paths as arguments. Remove automatic binding of data-firebase-value.

--- jquery.firebase.js
+++ jquery.firebase.js
@@ -1,29 +1,20 @@
 (function($) {
-	$.fn.firebase = function(extractor) {
-		extractor = extractor || function() { return $(this).data('firebase') || this.id; };
+	$.fn.firebase = function() {
 		var url = '';
 		var addPath = function() {
 			if(!(/^http/i.test(url))) {
-				var value = extractor.call(this);
-				if(value) url = (url.length ? (value + '/' + url) : value);
+				var data = $(this).data();
+				var key = _.find(_.keys(data), function(value) { return /^firebase/i.test(value); });
+				if(key) {
+					var value = data[key] || this.id;
+					if(value) {
+						url = (url.length ? (value + '/' + url) : value);
+					}
+				}
 			}
 		};
 		addPath.call(this);
-		$(this).parents('[data-firebase]').each(addPath);
-		return url.length ? new Firebase(url) : null;
+		$(this).parents().each(addPath);
+		return url.length ? (arguments.length ? Firebase.prototype.child.call(new Firebase(url), Array.prototype.slice.call(arguments, 0).join('/')) : new Firebase(url)) : null;
 	};
-
-	$(function() {
-		$('[data-firebase-value]').each(function() {
-			var $this = $(this);
-			var firebase = $this.firebase(function() {
-				return $(this).data('firebase-value') || $(this).data('firebase') || this.id;
-			});
-			if(firebase) {
-				firebase.on('value', function(snapshot) {
-					$this.html(snapshot.val());
-				});
-			}
-		});
-	});
 })(jQuery);
